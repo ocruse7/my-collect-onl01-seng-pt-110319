6bd4e4a66338cbd42efce0f95664e9b0451ab208
Owen Cruse <ocruse7@gmail.com>
2019-11-15 05:30:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect(array)
+  
+  if block_given?
+    index = 0
+    newArray = []
+    
+    while index < array.length do 
+      newArray << yield(array[index])
+      index += 1
+    end
+  else
+    puts "It's empty"
+  end
+  
+  newArray
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect( array ) do |name|
+  name.split(" ").first
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect( collection ) do |lang|
+   lang.upcase
+end
